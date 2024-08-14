0f729a758b75c0b789fa29d063f6c946282ac963
DoceAzedo <me@doceazedo.com>
2024-08-14 19:27:02 -0300

    Implement /paintings endpoint

--- app/controllers/paintings_controller.rb
+++ app/controllers/paintings_controller.rb
@@ -1,5 +1,22 @@
+require "nokogiri"
+
 class PaintingsController < ApplicationController
   def index
-    render json: { data: "Hello World!" }
+    html = File.read("files/van-gogh-paintings.html")
+    doc = Nokogiri::HTML5(html)
+    artworks = []
+
+    doc.css(".klitem").each_with_index do |item, idx|
+      data = {
+        "name" => item.css(".kltat")[0].content,
+        "extensions" => item.css(".klmeta").map { |meta| meta.content },
+        "link" => "https://www.google.com" + item["href"],
+        "image" => item.css("img")[0].attr("src")
+      }
+      artworks << data
+    end
+
+    # render html: html.html_safe
+    render json: { artworks: artworks }
   end
 end
